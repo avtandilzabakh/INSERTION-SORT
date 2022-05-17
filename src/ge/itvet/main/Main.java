8927b0f1f6375d94d58412814153a9c3da9d555c
avtandilzabakh <avtandilzabakhidze@itvet.ge>
2022-05-17 11:38:33 +0400

    INSERTION-SORT

---
+++ src/ge/itvet/main/Main.java
@@ -0,0 +1,25 @@
+package ge.itvet.main;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void sort(int... a) {
+        for (int j = 0; j < a.length; j++) {
+            int key = a[j];
+            int i = j - 1;
+            while (i >= 0 && a[i] > key) {
+                a[i + 1] = a[i];
+                i = i - 1;
+
+            }
+            a[i + 1] = key;
+        }
+        System.out.println(Arrays.toString(a));
+
+    }
+
+    public static void main(String[] args) {
+        sort(5, 2, 4, 6, 1, 3);
+
+    }
+}
